fde26003fc6ad84fbf86e0d2a7e7e2156229ddaa
Angela Louh <louh.angela@gmail.com>
2021-11-23 23:29:28 -0800

    Implemented BFS

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,3 +1,5 @@
+const Queue = require("./Queue");
+
 class BinarySearchTree {
   // This constructor represents a single node in the tree.
   // You can optionally pass in a key, a value, and a pointer to the parent node
@@ -198,4 +200,27 @@ class BinarySearchTree {
 
     return values;
   }
+
+  bfs(tree, values = [] ) {
+    const queue = new Queue();
+    queue.enqueue(tree); // Start the traversal at the tree and add the tree node to the queue to kick off the BFS
+    let node = queue.dequeue(); // Remove from the queue
+    while (node) {
+      values.push(node.value); // Add that value from the queue to an array
+
+      if (node.left) {
+        queue.enqueue(node.left); // Add the left child to the queue
+      }
+
+      if (node.right) {
+        queue.enqueue(node.right); // Add the right child to the queue
+      }
+
+      node = queue.dequeue();
+    }
+
+    return values;
+  }
+
+  
 }
